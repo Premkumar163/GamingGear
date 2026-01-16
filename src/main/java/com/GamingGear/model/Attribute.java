7c2e48b3a567e00f460042cbad5ec87b98c9ae5e
Premkumar163 <premkumar829107@gmail.com>
2026-01-16 18:25:06 +0530

    Attribute

---
+++ src/main/java/com/GamingGear/model/Attribute.java
@@ -0,0 +1,27 @@
+package com.GamingGear.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="attributes")
+@Getter@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Attribute {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	
+	private String name;
+}
